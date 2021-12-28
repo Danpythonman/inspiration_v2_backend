b7ee111e69c73a10f293c6205bdb3e4e433750e1
Daniel Di Giovanni <dannyjdigio@gmail.com>
2021-12-28 00:15:49 -0500

    Added database URI and database name as environment variables. Connected to database in index.js.

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 require("dotenv").config();
+const mongoose = require("mongoose");
 const express = require("express");
 
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: process.env.DB_NAME
+});
+
 const app = express();
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 app.listen(process.env.PORT | 5000, () => {
     console.log(`Listening on PORT ${process.env.PORT | 5000}...`);
