045a3767fd9ed23617f8b4cb1bda2740ba413291
Daniel Di Giovanni <dannyjdigio@gmail.com>
2021-12-29 23:26:53 -0500

    Added route for verifying email. This creates the verified user object and refresh and auth tokens.

--- controllers/userManagementController.js
+++ controllers/userManagementController.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcrypt");
 const nodemailer = require("nodemailer");
 const UnverifiedUserModel = require("../models/unverifiedUsers");
 const UserModel = require("../models/users");
+const jwt = require("jsonwebtoken");
 
 const signup = async (req, res) => {
     try {
@@ -63,6 +64,69 @@ const signup = async (req, res) => {
     }
 }
 
+const verifyAccount = async (req, res) => {
+    try{
+        // Make sure user has already registered email for verification
+        const unverifiedUser = await UnverifiedUserModel.findOne({ email: req.body.email });
+        if (!unverifiedUser) {
+            res.status(404).send(`Verification time exceeded, or ${req.body.email} has not registered for verification yet`);
+            return;
+        }
+
+        // Check user's verification code
+        if (!await bcrypt.compare(req.body.verificationCode, unverifiedUser.verificationHash)) {
+            res.status(400).send("Verification code invalid");
+            return;
+        }
+
+        /*
+         * CREATING AUTH AND REFRESH TOKENS
+         *
+         * The secret key for each token is the secret key from the environment variable
+         * concatenated with the secret key stored in their database entry.
+         *
+         * The secret key in their database entry is the hash of a random number
+         * from character 29 to the end of the string.
+         *
+         * (With bcrypt the hash part starts at character 29).
+         * (This is to keep the secret key from being very long).
+         */
+
+        // Create hash to use as part of the secret key of the auth token
+        const userAuthKey = crypto.randomInt(100000, 1000000).toString();
+        const userAuthHash = String(await bcrypt.hash(userAuthKey, 10)).substring(29);
+        // Create auth token
+        const authToken = jwt.sign(
+            { email: req.body.email },
+            process.env.JWT_AUTH_KEY + userAuthHash,
+            { expiresIn: process.env.AUTH_TOKEN_LIFESPAN }
+        );
+
+        // Create hash to use as part of the secret key of the refresh token
+        const userRefreshKey = crypto.randomInt(100000, 1000000).toString();
+        const userRefreshHash = String(await bcrypt.hash(userRefreshKey, 10)).substring(29);
+        // Create refresh token
+        const refreshToken = jwt.sign(
+            { email: req.body.email },
+            process.env.JWT_REFRESH_KEY + userRefreshHash,
+            { expiresIn: process.env.REFRESH_TOKEN_LIFESPAN }
+        );
+
+        await UserModel.create({
+            email: req.body.email,
+            name: req.body.name,
+            authTokenHash: userAuthHash,
+            refreshTokenHash: userRefreshHash,
+            lastLoginDate: Date.now()
+        });
+
+        res.status(201).send({ auth: authToken, refresh: refreshToken });
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
+
 module.exports = {
-    signup
+    signup,
+    verifyAccount
 };
--- routes/userManagementRoutes.js
+++ routes/userManagementRoutes.js
@@ -26,4 +26,35 @@ const userManagementController = require("../controllers/userManagementControlle
  */
 router.post("/signup", userManagementController.signup);
 
+/**
+ * @swagger
+ * /verify:
+ *   post:
+ *     tags:
+ *       - User Management
+ *     description: Verifies a user's email.
+ *     requestBody:
+ *       description: Object containing user's email, name, and verification code.
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             email:
+ *               type: string
+ *             name:
+ *               type: string
+ *             verificationCode:
+ *               type: string
+ *     responses:
+ *       200:
+ *         description: Email verified, database entry created.
+ *       400:
+ *         description: Verification code invalid.
+ *       404:
+ *         description: Verification time limit exceeded, or email has not been registered for verification.
+ *       500:
+ *         description: Internal server error
+ */
+router.post("/verify", userManagementController.verifyAccount);
+
 module.exports = router;
