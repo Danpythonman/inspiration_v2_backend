322ee2edcaed52b85034d7ec24bea1a0975871c0
Daniel Di Giovanni <dannyjdigio@gmail.com>
2021-12-30 17:02:20 -0500

    Defined JWT controller middlewear function for verifying auth token (it is extremely similar to the verify refresh token function; lots of repeated code).

--- controllers/jwtController.js
+++ controllers/jwtController.js
@@ -37,6 +37,43 @@ const verifyRefreshToken = async (req, res, next) => {
     }
 }
 
+const verifyAuthToken = async (req, res, next) => {
+    try {
+        const bearerHeader = req.headers.authorization;
+        if (!bearerHeader) {
+            res.setHeader("WWW-Authenticate", "Bearer");
+            res.status(401).send("Unauthorized - No token provided");
+            return;
+        }
+
+        // JWT has the form:
+        // Bearer <token>
+        // so to get the token part we split the string and the space and get the second part.
+        const token = bearerHeader.split(" ")[1];
+
+        const payload = jwtService.decodeToken(token);
+
+        req.body.user = await databaseService.getUserByEmail(payload.email);
+        if (!req.body.user) {
+            res.status(404).send(`User with email ${payload.email} not found`);
+            return;
+        }
+
+        try {
+            req.token = jwtService.verifyAuthToken(token, req.body.user.authTokenHash);
+        } catch (jwtError) {
+            res.setHeader("WWW-Authenticate", "Bearer");
+            res.status(401).send(`Unauthorized - ${jwtError.message}`);
+            return;
+        }
+
+        next();
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
+
 module.exports = {
+    verifyAuthToken,
     verifyRefreshToken
 };
