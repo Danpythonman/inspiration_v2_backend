5bd3977dfe04daec61ea919007fe7fd6f1b19bfc
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-01-02 16:52:21 -0500

    Added functions in the database service file for creating, updating, and getting the image of the day.

--- services/databaseService.js
+++ services/databaseService.js
@@ -1,5 +1,6 @@
 const VerificationRequestModel = require("../models/verificationRequests");
 const UserModel = require("../models/users");
+const ImageOfTheDayModel = require("../models/ImageOfTheDay");
 
 /**
  * Creates a user document in the database.
@@ -114,6 +115,56 @@ const deleteUser = async (email) => {
     return await UserModel.findOneAndDelete({ email: email });
 }
 
+/**
+ * Sets the image of the day document.
+ *
+ * @param {object} imageOfTheDayObject Object containing the image URL, title, photographer, and description.
+ * @param {string} imageOfTheDayObject.imageUrl The URL of the image.
+ * @param {string} imageOfTheDayObject.title The title of the image.
+ * @param {string} imageOfTheDayObject.photographer The photographer of the image.
+ * @param {string} imageOfTheDayObject.description The description of the image.
+ *
+ * @returns {Promise} The image of the day document in the database.
+ */
+ const createImageOfTheDay = async (imageOfTheDayObject) => {
+    return await ImageOfTheDayModel.create(imageOfTheDayObject);
+}
+
+/**
+ * Sets the image of the day document.
+ *
+ * @param {string} currentImageUrl The URL for the current image of the day.
+ * @param {object} newImageOfTheDayObject Object containing the URL, title, photographer, and description of the new image.
+ * @param {string} newImageOfTheDayObject.imageUrl The URL of the new image.
+ * @param {string} newImageOfTheDayObject.title The title of the new image.
+ * @param {string} newImageOfTheDayObject.photographer The photographer of the new image.
+ * @param {string} newImageOfTheDayObject.description The description of the nww image.
+ *
+ * @returns {Promise} The updated image of the day object in the database.
+ */
+const setImageOfTheDay = async (currentImageUrl, newImageOfTheDayObject) => {
+    return await ImageOfTheDayModel.findOneAndUpdate({ imageUrl: currentImageUrl }, newImageOfTheDayObject);
+}
+
+/**
+ * Gets the image of the day document from the database.
+ *
+ * @returns {Promise} The image of the day object. If nothing is found, undefined is returned.
+ */
+const getImageOfTheDay = async () => {
+    // There is only one image of the day document in the collection
+    // (when getting a new image this document is updated instead of adding more documents).
+    // This means that this will return an array with one document.
+    const imageArray = await ImageOfTheDayModel.find({});
+
+    // If the array is empty, the image of the day document was not found.
+    if (imageArray.length < 1) {
+        return undefined;
+    }
+    // If the array is not empty, then it has one element, which is the image of the day document.
+    return imageArray[0];
+}
+
 module.exports = {
     createUser,
     getUserByEmail,
@@ -122,5 +173,8 @@ module.exports = {
     deleteVerificationRequest,
     updateUserName,
     changeUserSecretHashes,
-    deleteUser
+    deleteUser,
+    createImageOfTheDay,
+    setImageOfTheDay,
+    getImageOfTheDay
 };
