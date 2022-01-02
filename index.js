f504ac2f90463614f64d7c7be5eeb57b0b8f29a2
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-01-02 13:32:50 -0500

    Installed cors npm package and added it to index.js.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
+const cors = require("cors");
 const userManagementRoutes = require("./routes/userManagementRoutes");
 
 mongoose.connect(process.env.DB_URI, {
@@ -10,6 +11,8 @@ mongoose.connect(process.env.DB_URI, {
 });
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
