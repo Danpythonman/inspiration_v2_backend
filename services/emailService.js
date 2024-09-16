14b0909314e00bceaa3f75d2345f605ec21e0044
Daniel Di Giovanni <dannyjdigio@gmail.com>
2024-09-16 16:50:59 -0400

    Replaced sendgrid emailer with VEA.

--- services/emailService.js
+++ services/emailService.js
@@ -1,5 +1,4 @@
 const nodemailer = require("nodemailer");
-const sgMail = require('@sendgrid/mail');
 
 /**
  * Sends verification code to specified email.
@@ -11,29 +10,29 @@ const sgMail = require('@sendgrid/mail');
  * @returns {Promise<undefined>}
  */
 const sendVerificationCode = async (emailRecipient, endpoint, verificationCode) => {
-    const emailMessage = {
-        from: process.env.EMAIL_SENDER,
-        to: emailRecipient,
-        subject: "Verification Code",
-        text: `Your ${endpoint} verification code is ${verificationCode} This verification code will expire in about 5 minutes. Do not share this code.`,
-        html: `
-            <table width="100%" border="0" cellspacing="0" cellpadding="0">
-                <tr>
-                    <td style="text-align: center;">
-                        <div style="font-family: Arial;">
-                            <h1>Your ${endpoint} Verification Code is</h1>
-                            <h1 style="font-size: 5rem">${verificationCode}</h1>
-                        </div>
-                    </td>
-                </tr>
-            </table>
-            <p style="font-family: Arial;">This verification code will expire in about 5 minutes. Do not share this code.</p>
-        `,
-    };
-
     // If the email environment variable is set to "test", then use the test email environment variables.
     // Otherwise, use SendGrid (for production).
     if (process.env.EMAIL_ENVIRONMENT === "test") {
+        const emailMessage = {
+            from: process.env.EMAIL_SENDER,
+            to: emailRecipient,
+            subject: "Verification Code",
+            text: `Your ${endpoint} verification code is ${verificationCode} This verification code will expire in about 5 minutes. Do not share this code.`,
+            html: `
+                <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tr>
+                        <td style="text-align: center;">
+                            <div style="font-family: Arial;">
+                                <h1>Your ${endpoint} Verification Code is</h1>
+                                <h1 style="font-size: 5rem">${verificationCode}</h1>
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+                <p style="font-family: Arial;">This verification code will expire in about 5 minutes. Do not share this code.</p>
+            `,
+        };
+
         const transporter = nodemailer.createTransport({
             host: process.env.TEST_EMAIL_HOST,
             port: process.env.TEST_EMAIL_PORT,
@@ -45,8 +44,26 @@ const sendVerificationCode = async (emailRecipient, endpoint, verificationCode) 
 
         await transporter.sendMail(emailMessage);
     } else {
-        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-        await sgMail.send(emailMessage);
+        const response = await fetch(
+            `https://${process.env.VEA_HOSTNAME}/custom/code/send`,
+            {
+                method: "post",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${process.env.VEA_AUTH_TOKEN}`
+                },
+                body: JSON.stringify({
+                    "email": emailRecipient,
+                    "code": verificationCode,
+                    "maximumAttempts": process.env.CODE_MAXIMUM_ATTEMPTS,
+                    "maximumDurationInMinutes": process.env.CODE_MAXIMUM_DURATION_IN_MINUTES
+                })
+            }
+        );
+
+        if (response.status != 200) {
+            throw new Error("Error sending verification code email.");
+        }
     }
 }
 
